a6fb1b0091caf0e007ceeb72518484692e827e92
Susana vik <susana.m.vik@gmail.com>
2021-02-10 04:42:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,8 @@ def monopoly_with_second_tier
   base_hash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {}
+      :rent_in_dollars => {},
+      :names => {}
     }
   }
   base_hash
