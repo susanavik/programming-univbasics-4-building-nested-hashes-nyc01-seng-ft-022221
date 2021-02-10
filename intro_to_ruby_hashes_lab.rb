d907e9873acbccc6342799506b34d33b6d00231c
Susana vik <susana.m.vik@gmail.com>
2021-02-10 04:30:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,11 @@
 def base_hash
   base_hash = {
-    railoads: []
+    railoads: {}
   }
 end
 
 def monopoly_with_second_tier
-  base_hash = {
-    :railoads => {}
-  }
+
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
