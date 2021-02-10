c759d89cf62c710438b1e98d025d0a4949cc48f1
Susana vik <susana.m.vik@gmail.com>
2021-02-10 04:49:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,7 @@ def monopoly_with_third_tier
         :four_pieces_owned => 200
       },
       :names => {
-        :reading_railroad => {
-          :mortgage_value => 100
-        },
+        :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
