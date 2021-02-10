f1aa7db5dfdcfbdf3f365b5e636b2453181676b4
Susana vik <susana.m.vik@gmail.com>
2021-02-10 04:44:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :one_piece_owned => []
+        :one_piece_owned => 25
       },
       :names => {}
     }
